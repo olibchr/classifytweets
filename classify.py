5463756eb9b118fb7511d1c35a1110d6e5b10957
Oliver Becher <oliver.becher@gmail.com>
2017-04-23 23:00:43 +0200

    push

--- classify.py
+++ classify.py
@@ -5,43 +5,71 @@ from flask import Flask, request, jsonify
 from dateutil import parser
 import matplotlib
 import sys
-app = Flask(__name__)
+from pumpkin import *
 
+###START-CONF
+##{
+##"object_name": "classify",
+##"object_poi": "my-classify-1234",
+##"auto-load" : false,
+##"parameters": [ {
+##                  "name": "tweets",
+##                  "description": "tweets to classify",
+##                  "required": true,
+##                  "type": "String",
+##                  "state" : "UNCLASSIFIED"
+##              } ],
+##"return": [
+##              {
+##                  "name": "predictions",
+##                  "description": "all predictions",
+##                  "required": true,
+##                  "type": "String",
+##                  "state" : "PREDICTED"
+##               }
+##
+##          ] }
+##END-CONF
+
+classifier = nltk.data.load("classifiers/movie_reviews_NaiveBayes.pickle")
+stop = set(stopwords.words('english'))
 
 class Tweet():
     def __init__(self, date, text):
         self.date = parser.parse(date)
         self.text = text
-        self.prediciton = None
+        self.prediction = None
         self.serialized = {}
     def serialize(self):
         self.serialized = {
             'date': self.date,
-            'text': self.text,
             'prediction': self.prediciton
         }
         return self.serialized
 
+class greet(PmkSeed.Seed):
+    def __init__(self, context, poi=None):
+        PmkSeed.Seed.__init__(self, context,poi)
+        pass
+    def run(self, pkt, tweets):
+        """ Data is transformed at intermediate points on its way
+        to a destination. In this case we are simply adding
+        "hello" to a name to form a greeting. This will be
+        dispatched and received by a collector.
+        """
+        data = json.loads(tweets)
+        tweets = []
+        for t in data:
+            tweets.append(Tweet(t['date'], t['tweet']))
+        for t in tweets:
+            print t
+            tokens = nltk.word_tokenize(t.text)
+            filtered_tokens = [i.lower() for i in tokens if i.lower() not in stop]
+            feats = dict([(word, True) for word in filtered_tokens])
+            t.prediction = classifier.classify(feats)
+        self.dispatch(pkt, [e.serialize() for e in tweets], "GREETING")
+        pass
 
-@app.route('/', methods=['POST'])
-def predict_class():
-    data = json.loads(request.form['tweets'])
-    tweets = []
-    for t in data:
-        tweets.append(Tweet(t['date'], t['tweet']))
-    for t in tweets:
-        print t
-        tokens = nltk.word_tokenize(t.text)
-        filtered_tokens = [i.lower() for i in tokens if i.lower() not in stop]
-        feats = dict([(word, True) for word in filtered_tokens])
-        t.prediciton = classifier.classify(feats)
-    return jsonify([e.serialize() for e in tweets]), 200, {'ContentType':'application/json'}
-
-
-if __name__ == '__main__':
-    classifier = nltk.data.load("classifiers/movie_reviews_NaiveBayes.pickle")
-    stop = set(stopwords.words('english'))
-    app.run(host='0.0.0.0', debug=True)
 
 
 
---
+++ plotting.py
@@ -0,0 +1,76 @@
+import nltk, json
+from nltk.corpus import stopwords
+import datetime, numpy, time
+from flask import Flask, request, jsonify
+from dateutil import parser
+import matplotlib.pyplot as plt
+import sys
+from pumpkin import *
+
+###START-CONF
+##{
+##"object_name": "plot",
+##"object_poi": "my-plot-1234",
+##"auto-load" : false,
+##"parameters": [ {
+##                  "name": "predictions",
+##                  "description": "predictions to plot",
+##                  "required": true,
+##                  "type": "String",
+##                  "state" : "UNCLASSIFIED"
+##              } ],
+##"return": [
+##
+##          ] }
+##END-CONF
+
+class Tweet():
+    def __init__(self, date, prediction):
+        self.date = parser.parse(date)
+        self.prediction = prediction
+
+class greet(PmkSeed.Seed):
+    def __init__(self, context, poi=None):
+        PmkSeed.Seed.__init__(self, context,poi)
+        pass
+    def run(self, pkt, predictions):
+        """ Data is transformed at intermediate points on its way
+        to a destination. In this case we are simply adding
+        "hello" to a name to form a greeting. This will be
+        dispatched and received by a collector.
+        """
+        data = json.loads(predictions)
+        tweets = []
+        for t in data:
+            tweets.append(Tweet(t['date'], t['prediction']))
+        dates = []
+        predictions = []
+        t = 0
+        while(t<len(tweets)):
+            subSet = tweets[t:len(tweets)/10+t]
+            this_date = 0
+            this_pred = []
+            for tweet in subSet:
+                this_pred += tweet.prediction
+                this_date += time.mktime(tweet.date)
+            this_pred = this_pred/len(subSet)
+            this_date = this_date/len(subSet)
+            dates.append(this_date)
+            predictions.append(this_pred)
+            t += len(subSet)
+        plt.plot(dates, predictions)
+        plt.savefig('predictions.png', bbox_inches='tight')
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
