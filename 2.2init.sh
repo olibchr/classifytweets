fbb0e9d059a8b3eff8d018e274da8ca57adeb36c
Oliver Becher <oliver.becher@gmail.com>
2017-04-23 23:31:01 +0200

    scrip

---
+++ 2.2init.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+sudo apt-get update
+wait
+sudo apt-get install python2.7
+wait
+sudo apt-get install python-virtualenv 
+wait
+cd ~
+sudo wget https://github.com/recap/pumpkin/archive/tracula.zip
+wait
+sudo apt-get install unzip
+unzip tracula.zip
+wait
+pip install netaddr
+wait
+pip install ujson
+wait
+pip install pika
+wait
+pip install pystun
+wait
+cd pumpkin-tracula
+sudo python setup.py install
+wait
+cd ~/
+git clone https://github.com/olibchr/classifytweets
+pip install nltk
+wait
+pip install numpy
+wait
+pip install dateutil
+wait
+python -m nltk.downloader all
+wait
+cd classifytweets
+
+
